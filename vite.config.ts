f92d51a723acd9a1d291abb31e89671d9c5f14bc
sergey-frey <sergey.frey.work@gmail.com>
2026-02-25 10:42:12 +0500

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/liz-app/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
